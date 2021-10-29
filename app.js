18cf2e17288f0620e2fa85f70078602223fd2b87
FettahAud <abooo2003adoany@gmail.com>
2021-10-29 16:02:32 +0300

    Add slid dots

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 const rightBut = document.querySelector('.right')
 const leftBut = document.querySelector('.left')
 const slides = document.querySelector('.slides')
+const dots = document.querySelectorAll('.dot')
+const activeDot = document.querySelector('.active')
 
-console.log(rightBut)
+// Some Var(s) 
 let right = 0
 slides.style.right = 0
-rightBut.addEventListener('click', () => {
+let currentSlide = 0;
+
+activeDot.style.left = dots[currentSlide + 1].offsetLeft + 'px'
+
+// Slide's Right Click
+rightBut.addEventListener('click', (e) => {
+    currentSlide++
     right += 80
     slides.style.right = `${right}vw`
     if (right === 320) {
         slides.style.right = 0
         right = 0
+        currentSlide = 0
     }
+    // Slide's Buttons
+    activeDot.style.left = dots[currentSlide + 1].offsetLeft + 'px'
 })
+// Slide's Left Click
 leftBut.addEventListener('click', () => {
     if (right === 0) {
         slides.style.right = 320
         right = 320
+        currentSlide = 4
     }
+    currentSlide--
     right -= 80
     slides.style.right = `${right}vw`
+    // Slide's Buttons
+    activeDot.style.left = dots[currentSlide + 1].offsetLeft + 'px'
 })
